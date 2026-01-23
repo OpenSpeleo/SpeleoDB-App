de5fb94bad85304d38f4998d9ac8c0c956a4acf7
Jonathan DEKHTIAR <jonathan@dekhtiar.com>
2026-01-23 18:22:46 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SpeleoDB',
+  webDir: 'dist'
+};
+
+export default config;
